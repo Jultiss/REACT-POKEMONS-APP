8992143089ccc9a707caf8dd266b9204a3aa60ab
Julien <julien.tissidre@gmail.com>
2023-09-17 18:28:44 +0200

    first commit

---
+++ build/precache-manifest.f78340d07d09774552c1aa5f7c63feaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f6e3f320861bfd0c98c49733899f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad92ea8c5642498992e",
+    "url": "/static/css/main.67ae47e6.chunk.css"
+  },
+  {
+    "revision": "5ad64a37bb465d21b573",
+    "url": "/static/js/2.29566f80.chunk.js"
+  },
+  {
+    "revision": "aad92ea8c5642498992e",
+    "url": "/static/js/main.131c96b0.chunk.js"
+  },
+  {
+    "revision": "29c74a5a220c8c203140",
+    "url": "/static/js/runtime-main.66b7cc92.js"
+  }
+]);
